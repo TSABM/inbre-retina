b5dd48c91fc70e562c52a677149ba30d703bd0d9
TSABM <97318325+TSABM@users.noreply.github.com>
2024-05-31 14:40:29 -0700

    Add files via upload

--- demo_application.py
+++ demo_application.py
@@ -23,6 +23,9 @@ class MainWindow(qtw.QWidget):
         controls = qtw.QWidget()
         controls.setLayout(qtw.QGridLayout())
 
+        menuBox = qtw.QWidget()
+        menuBox.setLayout(qtw.QHBoxLayout())
+
         #making the widgets
         opacityLabel = qtw.QLabel("Opacity")
         contrastLimitsLabel = qtw.QLabel("Contrast Limits")
@@ -31,14 +34,34 @@ class MainWindow(qtw.QWidget):
         colorLabel = qtw.QLabel("Color")
         blendingLabel = qtw.QLabel("Blending")
         interpolationLabel = qtw.QLabel("Interpolation")
+        
 
         opacityControl = qtw.QSlider(Qt.Horizontal)
+        opacityControl.setRange(0, 100)
+        opacityControl.setSliderPosition(100)
+        opacityControl.setSingleStep(1)
+        opacityControl.valueChanged['int'].connect(self.adjustOpacity)
+
         contrastLimitsControl = qtw.QSlider(Qt.Horizontal)
+        contrastLimitsControl.setRange(-100, 100)
+        contrastLimitsControl.setSliderPosition(0)
+        contrastLimitsControl.setSingleStep(1)
+        contrastLimitsControl.valueChanged['int'].connect(self.adjustContrastLimits)
+        
+
         autoContrastControl = qtw.QRadioButton()
+        
         gammaControl = qtw.QSlider(Qt.Horizontal)
+        gammaControl.setRange(-100, 100)
+        gammaControl.setSliderPosition(0)
+        contrastLimitsControl.setSingleStep(1)
+        gammaControl.valueChanged['int'].connect(self.adjustGamma)
+
         colorControl = qtw.QComboBox()
         blendingControl = qtw.QComboBox()
         interpolationControl = qtw.QComboBox()
+        fileExplorerButton = qtw.QPushButton("File")
+        saveButton = qtw.QPushButton("Save")
 
 
         #binding widgets to layout
@@ -58,16 +81,47 @@ class MainWindow(qtw.QWidget):
         controls.layout().addWidget(blendingControl, 5, 1)
         controls.layout().addWidget(interpolationControl, 6, 1)
 
+        menuBox.layout().addWidget(fileExplorerButton)
+        menuBox.layout().addWidget(saveButton)
+
         controlBox.layout().addWidget(controls)
         self.layout().addWidget(controlBox)
-    
+        controlBox.layout().addWidget(menuBox)
+
     def imageDisplay(self):
         self.label = qtw.QLabel(self)
-        self.pixmap = QPixmap('demoImage.jpg')
-        self.label.setPixmap(self.pixmap)
+        pixmap = QPixmap('demoImage.jpg')
+        if pixmap.isNull():
+            print("failed to load image")
+        self.label.setPixmap(pixmap)
         self.layout().addWidget(self.label)
         
-        
+    def loadImage(self):
+        print("unimplemented")
+    
+    def adjustOpacity(self, value):
+        print("opacity: ", value)
+    
+    def adjustContrastLimits(self, value):
+        print("contrast lim: ", value)
+
+    def toggleAutoContrast(self):
+        print("unimplemented")
+    
+    def adjustGamma(self):
+        print("unimplemented")
+
+    def adjustGamma(self):
+        print("unimplemented")
+    
+    def selectColor(self):
+        print("unimplemented")
+
+    def selectBlending(self):
+        print("unimplemented")
+    
+    def selectInterpolation(self):
+        print("unimplemented")
         
 
 app = qtw.QApplication([])
